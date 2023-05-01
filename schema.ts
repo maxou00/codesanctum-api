53bbe157a544648cd24975081c3534a1aa140373
themx05 <maximiliencomlan05@gmail.com>
2023-05-01 07:00:29 +0100

    fix: moved dev deploy to staging branch, added message entity for contact form

--- schema.ts
+++ schema.ts
@@ -4,8 +4,10 @@ import Post from './src/list/Post';
 import Tag from './src/list/Tag';
 import Comment from './src/list/Comment';
 import Reaction from './src/list/Reaction';
+import Message from './src/list/Message';
 
 export const lists: Lists = {
+  Message,
   User,
   Post,
   Tag,
