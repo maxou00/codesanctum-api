61bf9ced0f8310f5312595a964f3865a557b7f42
themx05 <maximiliencomlan05@gmail.com>
2023-05-05 18:51:32 +0100

    fix: added missing redirect uri

--- src/core/google.ts
+++ src/core/google.ts
@@ -2,9 +2,10 @@ import { OAuth2Client } from "google-auth-library";
 
 export const google = {
     clientId: process.env.GOOGLE_CLIENT_ID || "",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    redirectUri: "https://api-test.codesanctum.org/auth/google/callback"
 };
 
 export function getGoogleClient() {
-    return new OAuth2Client({ clientId: google.clientId, clientSecret: google.clientSecret });
+    return new OAuth2Client({ clientId: google.clientId, clientSecret: google.clientSecret, redirectUri: google.redirectUri });
 }
